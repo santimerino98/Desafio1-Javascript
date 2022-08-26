98b295eb04047dd72d6b16c7ee62d9045803fe57
santimerino98 <santiagomerino.98@gmail.com>
2022-08-26 13:43:05 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let score = prompt ('Ingrese la puntuación lograda');
+if(score>0 && score<=100){
+    while(score=true){
+    
+        if (score == 100){
+            alert('Puntuación máxima, ¡felicidades!');
+        }
+        else{
+            if(score>=70 && score<100){
+                alert('Puntuación muy buena, faltó poco');
+            }
+            else{
+                if(score>=1 && score<40){
+                    alert('Puntuación mala, mucho por mejorar');
+                }
+                else{
+                    if(score>=40 && score<70){
+                        alert('Puntuación regular, falta mejorar');
+                    }
+                }
+            }
+        }
+}
+}
